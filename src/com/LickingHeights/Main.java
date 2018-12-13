81a3fd441a98c004650b7c3699335070b553a610
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-12-13 08:50:33 -0500

    Beginning code
    I need to add either an array or something to get the ability to add more than 1 supply, figure that out ASAP

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,35 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	//is this working
+
+        //declare
+        Scanner keyboard;
+        double SupplyAmount;
+        double SalesDays;
+        double SupplyOneCost;
+
+        //initialize
+        keyboard = new Scanner (System.in);
+
+        System.out.println("How many different supplies are required?");
+        SupplyAmount = keyboard.nextDouble();
+
+        System.out.println("How many days of sales will there be?");
+        SalesDays = keyboard.nextDouble();
+
+
+        if (SupplyAmount==1) {
+            System.out.println("What's the cost of supply 1?");
+            SupplyOneCost = keyboard.nextDouble();
+        }
+
+
+
+
+
     }
 }
