3bf14ded1cbfdce7dd7449bce2854e07978f3733
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-12-14 08:10:37 -0500

    Step 2
    Need to initialize SupplyTotalCost some how, then move on to sales days, and profit stuff

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,11 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //declare
+        //declare *****
         Scanner keyboard;
         double SupplyAmount;
+
         double SalesDays;
+
         double SupplyOneCost;
+        double SupplyTwoCost;
+        double SupplyThreeCost;
+        double SupplyFourCost;
+        double SupplyFiveCost;
+        double SupplySixCost;
+        double SupplyCostTotal;
+        //declare end *****
+
+        String[] SupplyOptions;
+
 
         //initialize
         keyboard = new Scanner (System.in);
@@ -18,15 +30,82 @@ public class Main {
         System.out.println("How many different supplies are required?");
         SupplyAmount = keyboard.nextDouble();
 
+        if (SupplyAmount == 1) {
+            System.out.println("Input supply one cost.");
+            SupplyOneCost = keyboard.nextDouble();
+
+            SupplyCostTotal = SupplyOneCost;
+        }
+        else if (SupplyAmount == 2) {
+            System.out.println("Input supply one cost");
+            SupplyOneCost = keyboard.nextDouble();
+            System.out.println("Input supply two cost.");
+            SupplyTwoCost = keyboard.nextDouble();
+
+            SupplyCostTotal = SupplyOneCost + SupplyTwoCost;
+        }
+        else if (SupplyAmount == 3) {
+            System.out.println("Input supply one cost");
+            SupplyOneCost = keyboard.nextDouble();
+            System.out.println("Input supply two cost.");
+            SupplyTwoCost = keyboard.nextDouble();
+            System.out.println("Input supply three cost");
+            SupplyThreeCost = keyboard.nextDouble();
+
+            SupplyCostTotal = SupplyOneCost + SupplyTwoCost + SupplyThreeCost;
+        }
+        else if (SupplyAmount == 4) {
+            System.out.println("Input supply one cost");
+            SupplyOneCost = keyboard.nextDouble();
+            System.out.println("Input supply two cost.");
+            SupplyTwoCost = keyboard.nextDouble();
+            System.out.println("Input supply three cost");
+            SupplyThreeCost = keyboard.nextDouble();
+            System.out.println("Input supply four cost");
+            SupplyFourCost = keyboard.nextDouble();
+
+            SupplyCostTotal = SupplyOneCost + SupplyTwoCost + SupplyThreeCost + SupplyFourCost;
+        }
+        else if (SupplyAmount == 5) {
+            System.out.println("Input supply one cost");
+            SupplyOneCost = keyboard.nextDouble();
+            System.out.println("Input supply two cost.");
+            SupplyTwoCost = keyboard.nextDouble();
+            System.out.println("Input supply three cost");
+            SupplyThreeCost = keyboard.nextDouble();
+            System.out.println("Input supply four cost");
+            SupplyFourCost = keyboard.nextDouble();
+            System.out.println("Input supply five cost");
+            SupplyFiveCost = keyboard.nextDouble();
+
+            SupplyCostTotal = SupplyOneCost + SupplyTwoCost + SupplyThreeCost + SupplyFourCost + SupplyFiveCost;
+        }
+        else if (SupplyAmount == 6) {
+            System.out.println("Input supply one cost");
+            SupplyOneCost = keyboard.nextDouble();
+            System.out.println("Input supply two cost.");
+            SupplyTwoCost = keyboard.nextDouble();
+            System.out.println("Input supply three cost");
+            SupplyThreeCost = keyboard.nextDouble();
+            System.out.println("Input supply four cost");
+            SupplyFourCost = keyboard.nextDouble();
+            System.out.println("Input supply five cost");
+            SupplyFiveCost = keyboard.nextDouble();
+            System.out.println("Input supply six cost");
+            SupplySixCost = keyboard.nextDouble();
+
+            SupplyCostTotal = SupplyOneCost + SupplyTwoCost + SupplyThreeCost + SupplyFourCost + SupplyFiveCost + SupplySixCost;
+        }
+        
+
+
+
+
+
+
+
         System.out.println("How many days of sales will there be?");
         SalesDays = keyboard.nextDouble();
-
-
-        if (SupplyAmount==1) {
-            System.out.println("What's the cost of supply 1?");
-            SupplyOneCost = keyboard.nextDouble();
-        }
-
 
 
 
